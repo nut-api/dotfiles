73b0c6ba053bf9976fd27c5a21395e61bcafce98
nut-api <nut@apiplustech.com>
2026-01-08 16:45:58 +0700

    Add wezterm config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.enable_csi_u_key_encoding = true
+
+return config
